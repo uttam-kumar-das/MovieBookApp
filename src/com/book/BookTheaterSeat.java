f16f02f36bd5cf3769012f612f9e697f1d3ba3e0
uttam-kumar-das <duttam800@gmail.com>
2024-02-06 10:52:06 +0530

    write a msg

--- src/com/book/BookTheaterSeat.java
+++ src/com/book/BookTheaterSeat.java
@@ -1,5 +1,20 @@
 package com.book;
 
 public class BookTheaterSeat {
+	static int total_seats=20;
+	
+	synchronized void bookSeat(int seats) {
+		if(total_seats>seats) {
+			System.out.println(seats+" Seats booked successfully");
+			total_seats=total_seats-seats;
+			System.out.println("Seats left "+total_seats);
+		}
+		else {
+			System.out.println("Seats cannot be booked");
+			System.out.println("seat left "+total_seats);
+			System.out.println();
+			
+		}
+	}
 
 }
