a17322fb819aa48b78ea1f9fb1f6e490c402938c
uttam-kumar-das <duttam800@gmail.com>
2024-02-06 12:51:19 +0530

    created thread2

--- src/com/book/MyThread2.java
+++ src/com/book/MyThread2.java
@@ -1,5 +1,15 @@
 package com.book;
 
-public class MyThread2 {
+public class MyThread2 extends Thread {
+	BookTheaterSeat b;
+	int seats;
+	
+	MyThread2(BookTheaterSeat b, int seats){
+		this.b=b;
+		this.seats=seats;
+	}
+	public void run() {
+		b.bookSeat(seats);
+	}
 
 }
