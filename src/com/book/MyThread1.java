a65bc2ec5cc2db3e907e78e17f69cde4376bb0e8
uttam-kumar-das <duttam800@gmail.com>
2024-02-06 11:16:47 +0530

    extends thread and written a constructor and initialize it

--- src/com/book/MyThread1.java
+++ src/com/book/MyThread1.java
@@ -1,5 +1,16 @@
 package com.book;
 
-public class MyThread1 {
-
+public class MyThread1 extends Thread {
+	
+	BookTheaterSeat b;
+	int seats;
+	MyThread1(BookTheaterSeat b, int seats){
+		this.b=b;
+		this.seats=seats;
+	}
+	public void run() {
+		b.bookSeat(seats);
+	}
+	
+	
 }
